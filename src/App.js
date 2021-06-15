fe06840c1ac45f2d604dc9c7c1d108afb97f3492
Daniele Cioffi <daniele.cioffi.stud@gmail.com>
2021-06-15 14:33:17 +0200

    fix: creazione tabella con valori statici

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 class App {
   constructor() {
-    document.write("test")
+    let lista = [
+      new Film("Film 1", "Azione", "2020"),
+      new Film("Film 2", "Thriller", "2021"),
+      new Film("Film 3", "Horror", "2022"),
+      new Film("Film 4", "Drammatico", "2023"),
+      new Film("Film 5", "Comico", "2024"),
+      new Film("Film 6", "Romantico", "2025"),
+    ];
+
+    let obj = '<ul class="list-group">';
+
+    for (let i = 0; i < lista.length; i++) {
+      obj += '<li class="list-group-item">' + lista[i].toString() + "</li>";
+    }
+
+    obj += "</ul>";
+
+    document.getElementById("lista").innerHTML = obj;
   }
+}
+
+class Film {
+  constructor(titolo, genere, anno) {
+    this.titolo = titolo;
+    this.genere = genere;
+    this.anno = anno;
+  }
+
+  toString() {
+    return this.titolo + " " + this.genere + " " + this.anno;
+  }
+
+  titolo;
+  genere;
+  anno;
 }
 
 const app = new App();
