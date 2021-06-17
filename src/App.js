a0658d9e23906c79bdb9db0933948129d02c9862
Daniele Cioffi <daniele.cioffi.stud@gmail.com>
2021-06-17 15:48:46 +0200

    style: cambiato la fetch e migliorato stringa obj

--- src/App.js
+++ src/App.js
@@ -5,47 +5,47 @@ class App {
 
   //initializes the list using fetch from a server
   async initList() {
-    const req = await fetch(
+    fetch(
       "https://api.themoviedb.org/3/movie/popular?api_key=c4d79d0d1e50bf8bc86b7afbd240e4df&language=en&page=1"
-    );
-    const data = await req.json();
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        let list = [];
+        let tmp;
 
-    let list = [];
-    let tmp;
+        for (let i = 0; i < data.results.length; i++) {
+          tmp = data.results[i];
+          list.push(
+            new Movie(
+              tmp.title,
+              tmp.overview,
+              tmp.poster_path,
+              tmp.release_date
+            )
+          );
+        }
 
-    for (let i = 0; i < data.results.length; i++) {
-      tmp = data.results[i];
-      list.push(
-        new Movie(tmp.title, tmp.overview, tmp.poster_path, tmp.release_date)
-      );
-    }
-
-    this.list = list;
-    this.showCards(this.list);
+        this.list = list;
+        this.showCards(this.list);
+      });
   }
 
   //Shows the bootstrap cards using the values from the parameter of the function
   showCards(values) {
-    //let obj = "<div id=\"films\" class='row'>";
     let obj = "";
     for (let i = 0; i < values.length; i++) {
-      obj += '<div class="card">';
-      obj +=
-        '<img class="card-img-top" src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/' +
-        values[i].poster_path +
-        '"alt="Card image cap"></img>';
-      obj += '<div class="card-body">';
-      obj += '<h5 class="card-title">' + values[i].title + "</h5>";
-      obj += '<p class="card-text">' + values[i].overview + "</p>";
-      obj += '<div class="btn-div">';
-      obj +=
-        '<a href="#" class="btn btn-primary" id="detailsButton">Details</a>';
-      obj += "</div>";
-      obj += "</div>";
-      obj += "</div>";
+      obj += `<div class="card">
+        <img class="card-img-top" src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${values[i].poster_path}"
+        alt="Card image cap"></img>
+        <div class="card-body">
+        <h5 class="card-title">${values[i].title}</h5>
+        <p class="card-text">${values[i].overview}</p>
+        <div class="btn-div">
+        <a href="#" class="btn btn-primary" id="detailsButton">Details</a>
+        </div>
+        </div>
+        </div>`;
     }
-
-    //obj += "</div>";
 
     document.getElementById("films").innerHTML = obj;
   }
